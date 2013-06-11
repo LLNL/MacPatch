d7fb42fc304126794c0ffec8b67d881012e51c7c
Charles Heizer <heizer1@llnl.gov>
2013-06-11 09:51:30 -0700

    no message

--- MacPatch Server/Server/conf/scripts/Permissions.sh
+++ MacPatch Server/Server/conf/scripts/Permissions.sh
@@ -7,6 +7,7 @@
 chown -R root:admin /Library/MacPatch/Server
 chown -R 79:70 /Library/MacPatch/Server/jetty-mpwsl
 chown -R 79:70 /Library/MacPatch/Server/jetty-mpsite
+chown -R 79:70 /Library/MacPatch/Server/Logs
 chown -R 79:70 /Library/MacPatch/Content/Web
 chmod 0775 /Library/MacPatch/Server
 chmod -R 0775 /Library/MacPatch/Content/Web
