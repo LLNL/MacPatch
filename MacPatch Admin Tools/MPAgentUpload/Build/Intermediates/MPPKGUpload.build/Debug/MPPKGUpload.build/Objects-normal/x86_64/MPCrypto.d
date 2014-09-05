8ec53d1071f8ccbf1c01c071264ae973a1ab9e86
Charlie Heizers <heizer1@llnl.gov>
2014-09-05 10:21:29 -0700

    Update GitIgnore

--- MacPatch Admin Tools/MPAgentUpload/Build/Intermediates/MPPKGUpload.build/Debug/MPPKGUpload.build/Objects-normal/x86_64/MPCrypto.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/heizer1/GitHub/MP_2.5.0_BETA/MacPatch/MacPatch\ Admin\ Tools/MPAgentUpload/MPPKGUpload/MPCrypto.m \
-  /Users/heizer1/GitHub/MP_2.5.0_BETA/MacPatch/MacPatch\ Admin\ Tools/MPAgentUpload/MPPKGUpload/MPCrypto.h
--- MacPatch Admin Tools/MPAgentUpload/Build/Intermediates/MPPKGUpload.build/Debug/MPPKGUpload.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/heizer1/GitHub/MP_2.5.0_BETA/MacPatch/MacPatch\ Admin\ Tools/MPAgentUpload/MPPKGUpload/main.m
