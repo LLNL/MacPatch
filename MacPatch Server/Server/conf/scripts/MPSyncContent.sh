1c57bd54967d644eb3a807f0a7e8511953742597
Charlie Heizers <heizer1@llnl.gov>
2014-09-05 08:54:06 -0700

    Update Rsync to Support Linux
    Increment Client Build No.

--- MacPatch Server/Server/conf/scripts/MPSyncContent.sh
+++ MacPatch Server/Server/conf/scripts/MPSyncContent.sh
@@ -1,22 +1,77 @@
 #!/bin/bash
 #
 # --------------------------------------------------------------
-# Script mpContentSync.sh
-# Version 1.0
+# Script MPSyncContent.sh
+# Version 1.1.0
 # Edit: MASTER_SERVER variable
 #
-# Runs via LaunchDaemon on Interval
+# Runs via LaunchDaemon on Interval or Cron Job
 #
 # --------------------------------------------------------------
 
+
+# Rsync Path
 SYNC_DIR_NAME="mpContentWeb"
+# Rsync Server 
 MASTER_SERVER="localhost"
+# Sync Content to...
 LOCAL_CONTENT="/Library/MacPatch/Content/Web"
 
+MP_SRV_BASE="/Library/MacPatch/Server"
+MP_SRV_CONF="${MP_SRV_BASE}/conf"
+MP_SYNC_PLIST="${MP_SRV_CONF}/etc/gov.llnl.mp.sync.plist"
+
+USE_SERVER=false
+USE_PLIST=true
+
+function usage
+{
+	echo
+    echo "usage: MPSyncContent.sh [-s server] | [-p plist] | -h"
+    echo
+    echo "  -s   --server    Rsync Server to Sync from"
+    echo "  -p   --plist     Plist containing config data"
+    echo 
+    echo "  -h   --help      Help or Usage"
+    echo
+}
+
+
+# Process Args
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --server ) 	shift
+                            MASTER_SERVER=$1
+                            USE_SERVER=true
+                            ;;
+        -p | --plist )	 	shift
+                            MP_SYNC_PLIST=$1
+                            ;;
+        -h | --help )       usage
+                            exit
+                            ;;
+    esac
+    shift
+done
+
 # Dont Run Unless Master Server Is Configured
+
+if [ $USE_SERVER == false ]; then
+
+	if [ ! -f $MP_SYNC_PLIST ]; then
+		echo "Error, $MP_SYNC_PLIST was not found."
+		exit 1
+	fi
+
+	MASTER_SERVER = `defaults read $MP_SYNC_PLIST MPServerAddress`
+fi
+
 if [ "$MASTER_SERVER" != "localhost" ]; then
 	echo "$(/bin/date +"%Y-%m-%d %H:%M:%S") --- Starting Content Sync..."
 	/usr/bin/rsync -vai --delete-before --ignore-errors --exclude=.DS_Store \
 	$MASTER_SERVER::$SYNC_DIR_NAME $LOCAL_CONTENT
 	echo "$(/bin/date +"%Y-%m-%d %H:%M:%S") --- Content Sync Complete"
+else
+	echo "Error, localhost is not supported."
 fi
---
+++ MacPatch Server/Server/conf/scripts/Setup/RsyncServerSetup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+'''
+ Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+ Produced at the Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ Written by Charles Heizer <heizer1 at llnl.gov>.
+ LLNL-CODE-636469 All rights reserved.
+ 
+ This file is part of MacPatch, a program for installing and patching
+ software.
+ 
+ MacPatch is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License (as published by the Free
+ Software Foundation) version 2, dated June 1991.
+ 
+ MacPatch is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU General Public
+ License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with MacPatch; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+'''
+
+'''
+  MacPatch Rsync Service Setup Script
+  MacPatch Version 2.5.x
+  
+  Script Version 1.1.0
+'''
+
+import os
+import plistlib
+import platform
+
+MP_SRV_BASE = "/Library/MacPatch/Server"
+MP_SRV_CONF = MP_SRV_BASE+"/conf"
+OS_TYPE = platform.system()
+system_name = platform.uname()[1]
+dist_type = platform.dist()[1]
+
+'''	
+# ----------------------------------	
+# Script Requires ROOT
+# ----------------------------------
+'''
+if os.geteuid() != 0:
+    exit("\nYou must be an admin user to run this script.\nPlease re-run the script using sudo.\n")
+
+print("This Script will setup this server to syncronize data from the \"Master\" MacPatch server.\n\n")
+
+server_name = raw_input("MacPatch Server name TO sync data from [" + str(system_name) + "]:") or str(system_name)
+
+'''	
+# ----------------------------------	
+# Write the Plist With Changes	
+# ----------------------------------
+'''
+theFile = MP_SRV_BASE + "/conf/etc/gov.llnl.mp.sync.plist"
+prefs = plistlib.readPlist(theFile)
+prefs['MPServerAddress'] = server_name
+
+try:
+	plistlib.writePlist(prefs,theFile)	
+except Exception, e:
+	print("Error: %s" % e)	
+
+
+'''	
+# ----------------------------------	
+# Enable Startup Scripts
+# ----------------------------------
+'''
+if OS_TYPE == "Darwin":
+	if os.path.exists("/Library/MacPatch/Server/conf/LaunchDaemons/gov.llnl.mp.sync.plist"):
+		if os.path.exists("/Library/LaunchDaemons/gov.llnl.mp.sync.plist"):
+			os.remove("/Library/LaunchDaemons/gov.llnl.mp.sync.plist")
+		
+		os.symlink("/Library/MacPatch/Server/conf/LaunchDaemons/gov.llnl.mp.sync.plist","/Library/LaunchDaemons/gov.llnl.mp.sync.plist")
+		os.chown("/Library/MacPatch/Server/conf/LaunchDaemons/gov.llnl.mp.sync.plist", 0, 0)
+		os.chmod("/Library/MacPatch/Server/conf/LaunchDaemons/gov.llnl.mp.sync.plist", 0644)
+
+if OS_TYPE == "Linux":
+	from crontab import CronTab
+	cron = CronTab()
+	job  = cron.new(command='/Library/MacPatch/Server/conf/scripts/MPSyncContent.sh -p /Library/MacPatch/Server/conf/etc/gov.llnl.sync.plist -r')
+	job.set_comment("MPSyncContent")
+	job.hour.every(11)	
