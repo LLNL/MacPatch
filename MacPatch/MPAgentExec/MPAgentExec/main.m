78850d714ee20bbfdc897248690c55fa16418b82
Charlie Heizers <heizer1@llnl.gov>
2014-10-08 14:23:21 -0700

    Fixed Issue With Argument Type

--- MacPatch/MPAgentExec/MPAgentExec/main.m
+++ MacPatch/MPAgentExec/MPAgentExec/main.m
@@ -88,7 +88,7 @@ int main (int argc, char * argv[])
             };
             // getopt_long stores the option index here.
             int option_index = 0;
-            c = getopt_long (argc, argv, "Dsuf:B:aUgGCSiFt:Acg:d:P:eVvh", long_options, &option_index);
+            c = getopt_long (argc, argv, "Dsuf:B:aUGCSiFt:Acg:d:P:eVvh", long_options, &option_index);
 
             // Detect the end of the options.
             if (c == -1) {
