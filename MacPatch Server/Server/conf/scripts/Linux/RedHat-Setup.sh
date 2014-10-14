96c2cf165d5464ebbff28d91bbc7b5a5edd5ad37
Charlie Heizers <heizer1@llnl.gov>
2014-10-14 15:42:00 -0700

    Updates & Bug Fixes

--- MacPatch Server/Server/conf/scripts/Linux/RedHat-Setup.sh
+++ MacPatch Server/Server/conf/scripts/Linux/RedHat-Setup.sh
@@ -9,5 +9,8 @@ LIST_OF_APPS="gcc-c++ openssl-devel java-1.7.0-openjdk-devel libxml2-devel bzip2
 yum -y install $LIST_OF_APPS
 
 # Python Modules
+pip install plistlib
+pip install biplist
 pip install requests
 pip install python-crontab
+pip install argparse
