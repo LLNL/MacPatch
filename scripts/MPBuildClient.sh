fb8ba6d516f7b7b51860e3f8e3d8e0983b1f6395
Charlie Heizer <heizer1@llnl.gov>
2015-07-08 14:28:55 -0700

    Updated Client Build Script to support code signing.

--- scripts/MPBuildClient.sh
+++ scripts/MPBuildClient.sh
@@ -2,7 +2,7 @@
 #
 # -------------------------------------------------------------
 # Script: MPBuildClient.sh
-# Version: 1.0
+# Version: 1.1
 #
 # Description:
 # This is a very simple script to demonstrate how to automate
@@ -12,21 +12,43 @@
 # Simply modify the GITROOT and BUILDROOT variables also
 # please set the component package version numbers as well.
 #
+# History:
+#	1.1		Added Code Signbing Support
+#
 # -------------------------------------------------------------
 
 GITROOT="/Library/MacPatch/tmp/MacPatch"
 BUILDROOT="/Library/MacPatch/tmp/Client"
 BASEPKGVER="2.7.0.0"
 UPDTPKGVER="2.7.0.0"
+CODESIGNIDENTITY="*"
 
 if [ -d "$BUILDROOT" ]; then
 	rm -rf ${BUILDROOT}
 else
 	mkdir -p ${BUILDROOT}	
 fi	
+echo "A valid code siginging identidy is required."
+read -p "Would you like to code sign all binaries (Y/N)? [N]: " SIGNCODE
+SIGNCODE=${SIGNCODE:-N}
+fi
 
-# Compile the agent components
-xcodebuild clean build -configuration Release -project ${GITROOT}/MacPatch/MacPatch.xcodeproj -target AGENT_BUILD SYMROOT=${BUILDROOT}
+if [ "$SIGNCODE" == "n" ] || [ "$SIGNCODE" == "N" ] || [ "$SIGNCODE" == "y" ] || [ "$SIGNCODE" == "Y" ]; then
+
+	if [ "$SIGNCODE" == "y" ] || [ "$SIGNCODE" == "Y" ] ; then
+		# Compile the agent components
+		if [ "${CODESIGNIDENTITY}" == "*" ]; then
+			read -p "Please enter you code sigining identity: " CODESIGNIDENTITY
+		fi
+		xcodebuild clean build -configuration Release -project ${GITROOT}/MacPatch/MacPatch.xcodeproj -target AGENT_BUILD SYMROOT=${BUILDROOT} CODE_SIGN_IDENTITY="*"
+	else
+		# Compile the agent components
+		xcodebuild clean build -configuration Release -project ${GITROOT}/MacPatch/MacPatch.xcodeproj -target AGENT_BUILD SYMROOT=${BUILDROOT}
+	fi
+else
+	echo "Invalid entry, now exiting."
+	exit 1
+fi
 
 # Remove the build and symbol files
 find ${BUILDROOT} -name "*.build" -print | xargs -I{} rm -rf {}
