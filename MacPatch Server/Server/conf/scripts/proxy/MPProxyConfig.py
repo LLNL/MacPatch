347e08cc20afd48cb423295ddce79d00f263ef4f
Charlie Heizer <heizer1@llnl.gov>
2015-07-14 16:16:47 -0700

    Bug Fix

--- MacPatch Server/Server/conf/scripts/proxy/MPProxyConfig.py
+++ MacPatch Server/Server/conf/scripts/proxy/MPProxyConfig.py
@@ -29,16 +29,18 @@
 '''
 
 import os
+import sys
 import json
 import plistlib
 import biplist
 import platform
 from pprint import pprint
 import argparse
+import commands
 
 MP_SRV_BASE = "/Library/MacPatch/Server"
 MP_SRV_CONF = MP_SRV_BASE+"/conf"
-proxy_services = ['gov.llnl.mp.httpd.plist', 'gov.llnl.mp.proxy.plist', 'gov.llnl.mp.ProxySync.plist'];
+proxy_services = ['gov.llnl.mp.httpd.plist', 'gov.llnl.mp.proxy.plist', 'gov.llnl.mp.ProxySync.plist']
 OS_TYPE = platform.system()
 system_name = platform.uname()[1]
 dist_type = platform.dist()[1]
@@ -49,26 +51,29 @@ dist_type = platform.dist()[1]
 def setup_startup_scripts(services):
     if OS_TYPE == "Darwin":
         for item in services:
-            sys_file = "/LaunchDaemons/" item
+            sys_file = "/Library/LaunchDaemons/" + item
             mp_file = MP_SRV_CONF + sys_file
 
-        	if os.path.exists(mp_file):
-        		if os.path.exists(sys_file):
-        			os.remove(sys_file)
+            if os.path.exists(mp_file):
+                if os.path.exists(sys_file):
+                    os.remove(sys_file)
 
-        		os.chown(mp_file, 0, 0)
-        		os.chmod(mp_file, 0644)
+                os.chown(mp_file, 0, 0)
+                os.chmod(mp_file, 0644)
                 os.symlink(mp_file,sys_file)
+            else:
+                print "Error, %s not found" % mp_file
+                exit()
 
     if OS_TYPE == "Linux":
         print("Linux is not supported yet.")
 
         '''
-    	from crontab import CronTab
-    	cron = CronTab()
-    	job  = cron.new(command='/Library/MacPatch/Server/conf/scripts/MPAVDefsSync.py -p /Library/MacPatch/Server/conf/etc/gov.llnl.mpavdl.plist -r')
-    	job.set_comment("MPAVLoader")
-    	job.hour.every(11)
+        from crontab import CronTab
+        cron = CronTab()
+        job  = cron.new(command='/Library/MacPatch/Server/conf/scripts/MPAVDefsSync.py -p /Library/MacPatch/Server/conf/etc/gov.llnl.mpavdl.plist -r')
+        job.set_comment("MPAVLoader")
+        job.hour.every(11)
         '''
 
 # -----------------------------------
@@ -76,50 +81,50 @@ def setup_startup_scripts(services):
 # -----------------------------------
 
 def downloadAndAddCertForServers (servers):
-	# Input is server:port
-	if not os.path.exists(MP_SRV_CONF+"/jsseCerts"):
-		os.makedirs(MP_SRV_CONF+"/jsseCerts")
+    # Input is server:port
+    if not os.path.exists(MP_SRV_CONF+"/jsseCerts"):
+        os.makedirs(MP_SRV_CONF+"/jsseCerts")
 
-	# Make a list of the server input arg
-	_servers = []
-	_servers = servers.split(' ')
+    # Make a list of the server input arg
+    _servers = []
+    _servers = servers.split(' ')
 
-	# Download and add the cert for each server
-	for server in _servers:
-		addCert(server)
+    # Download and add the cert for each server
+    for server in _servers:
+        addCert(server)
 
-	print
-	print "Certificates have been download, and a jssecacerts file has been created."
-	print
-	print "****************** NOTE ******************"
-	print "A reboot of the J2EE services will be requirerd for the application server to recognize the new cert(s)."
-	print
+    print
+    print "Certificates have been download, and a jssecacerts file has been created."
+    print
+    print "****************** NOTE ******************"
+    print "A reboot of the J2EE services will be requirerd for the application server to recognize the new cert(s)."
+    print
 
 def addCert(host):
 
-	fileName = host.replace(':','_')
-	hostNameShort = fileName.split('.')[0]
+    fileName = host.replace(':','_')
+    hostNameShort = fileName.split('.')[0]
 
-	if os.path.exists(MP_SRV_CONF+"/jsseCerts/"+host+".cer"):
-		os.remove(MP_SRV_CONF+"/jsseCerts/"+host+".cer")
+    if os.path.exists(MP_SRV_CONF+"/jsseCerts/"+host+".cer"):
+        os.remove(MP_SRV_CONF+"/jsseCerts/"+host+".cer")
 
-	# Download the new cert
-	(ret, out) = commands.getstatusoutput('echo | openssl s_client -connect '+host+' 2>/dev/null | sed -ne \'/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p\' > "'+MP_SRV_CONF+'/jsseCerts/'+fileName+'.cer"')
-	if ret == 0:
-		print "Import "+host+" to jssecacerts"
-		# if the jssecacerts file does not exist dont delete anything
-		if os.path.exists(MP_SRV_CONF+'/jsseCerts/jssecacerts'):
-			# Delete the alias if it exists, it's easier to just delete and re-add it
-			(delRet, delOut) = commands.getstatusoutput('keytool -delete -alias "'+hostNameShort+'" -keystore "'+MP_SRV_CONF+'/jsseCerts/jssecacerts" -storepass changeit -trustcacerts -noprompt')
-			if (delRet != 0):
-				print "Error deleting alias for "+hostNameShort+ " " +delOut
+    # Download the new cert
+    (ret, out) = commands.getstatusoutput('echo | openssl s_client -connect '+host+' 2>/dev/null | sed -ne \'/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p\' > "'+MP_SRV_CONF+'/jsseCerts/'+fileName+'.cer"')
+    if ret == 0:
+        print "Import "+host+" to jssecacerts"
+        # if the jssecacerts file does not exist dont delete anything
+        if os.path.exists(MP_SRV_CONF+'/jsseCerts/jssecacerts'):
+            # Delete the alias if it exists, it's easier to just delete and re-add it
+            (delRet, delOut) = commands.getstatusoutput('keytool -delete -alias "'+hostNameShort+'" -keystore "'+MP_SRV_CONF+'/jsseCerts/jssecacerts" -storepass changeit -trustcacerts -noprompt')
+            if (delRet != 0):
+                print "Error deleting alias for "+hostNameShort+ " " +delOut
 
-		# Add the downloaded cert
-		(addRet, addOut) = commands.getstatusoutput('keytool -import -file "'+MP_SRV_CONF+'/jsseCerts/'+fileName+'.cer" -alias "'+hostNameShort+'" -keystore "'+MP_SRV_CONF+'/jsseCerts/jssecacerts" -storepass changeit -trustcacerts -noprompt')
-		if (addRet != 0):
-			print "Error adding cert for "+hostNameShort+ " " +addOut
-	else:
-		print "Error getting cert for "+host+". "+out
+        # Add the downloaded cert
+        (addRet, addOut) = commands.getstatusoutput('keytool -import -file "'+MP_SRV_CONF+'/jsseCerts/'+fileName+'.cer" -alias "'+hostNameShort+'" -keystore "'+MP_SRV_CONF+'/jsseCerts/jssecacerts" -storepass changeit -trustcacerts -noprompt')
+        if (addRet != 0):
+            print "Error adding cert for "+hostNameShort+ " " +addOut
+    else:
+        print "Error getting cert for "+host+". "+out
 
 # -----------------------------------
 # Add Server Info to plist
@@ -192,61 +197,62 @@ def ConfigureServer():
     theFile = MP_SRV_BASE + "/conf/etc/gov.llnl.MPProxySync.plist"
     isBinPlist = isBinaryPlist(theFile)
 
-    if isBinPlist === True:
-    	prefs = biplist.readPlist(theFile)
+    if isBinPlist == True:
+        prefs = biplist.readPlist(theFile)
     else:
-    	prefs = plistlib.readPlist(theFile)
+        prefs = plistlib.readPlist(theFile)
 
     # Apply The Settings
     prefs['MPServerAddress'] = server_name
     prefs['MPServerPort'] = str(server_port)
 
     try:
-    	if isBinPlist === True:
-    		biplist.writePlist(prefs,theFile)
-    	else:
-    		plistlib.writePlist(prefs,theFile)
+        if isBinPlist == True:
+            biplist.writePlist(prefs,theFile)
+        else:
+            plistlib.writePlist(prefs,theFile)
     except Exception, e:
-    	print("Error: %s" % e)
+        print("Error: %s" % e)
 
 # ------------------------------
 # Main Methods
 # ------------------------------
 def main():
     # Args Parser
-	parser = argparse.ArgumentParser(description='Process Arguments for MP Proxy Server Setup')
-	parser.add_argument('--setup', help='Configure MPProxy Server', action='store_false')
-	parser.add_argument('--services', help="All | http | server | sync", required=False, default="none")
+    parser = argparse.ArgumentParser(description='Process Arguments for MP Proxy Server Setup')
+    parser.add_argument('--setup', help='Configure MPProxy Server', action='store_false')
+    parser.add_argument('--services', help="All | http | server | sync", required=False, default="none")
     parser.add_argument('--action', help="start | stop", required=False, default="none")
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	try:
+    try:
         # ----------------------------------
         # Script Requires ROOT
         # ----------------------------------
         os.system('clear')
         if os.geteuid() != 0:
-            exit("\nYou must be an admin user to run this script.\nPlease re-run the script using sudo.\n")
+            print "GO"
+            #exit("\nYou must be an admin user to run this script.\nPlease re-run the script using sudo.\n")
 
         if OS_TYPE == "Linux":
             exit("\nLinux is not supported yet.\n")
 
         # Setup
-		if args.setup:
-			ConfigureServer()
+        if args.setup:
+            ConfigureServer()
             start_services = raw_input("Use SSL for MacPatch connection [Y]:") or "Y"
             if start_services == "Y":
-            	args.services='All'
+                args.services='All'
                 args.action='start'
             else:
-            	print("To start the proxy server services run the following command.")
+                print("To start the proxy server services run the following command.")
                 print("MPProxyConfig.py --services All --action start")
                 sys.exit(0)
 
         # Service control
         if args.services != 'none' and args.action != 'none':
             the_services = []
-    		if args.services == 'All':
+            if args.services == 'All':
                 the_services.extend(proxy_services)
             elif args.services == 'http':
                 the_services.append(proxy_services[0])
@@ -260,9 +266,9 @@ def main():
             # Start or Stop the services
             serviceControl(args.action,the_services)
 
-	except Exception, e:
-	    print "%s" % e
-	    sys.exit(1)
+    except Exception, e:
+        print "%s" % e
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
