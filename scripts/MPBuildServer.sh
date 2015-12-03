c0405495b55ebe3163dedfb62b716d1f09453492
Heizer, Charles <heizer1@llnl.gov>
2015-12-03 14:27:35 -0800

    Update Server Build

--- scripts/MPBuildServer.sh
+++ scripts/MPBuildServer.sh
@@ -72,6 +72,30 @@ else
 fi
 
 # -----------------------------------
+# Java Check
+# -----------------------------------
+
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" > "1.8" ]]; then
+        echo version is more than 1.8
+    else         
+        echo version is less than 1.8
+    fi
+fi
+
+# -----------------------------------
 # Main
 # -----------------------------------
 clear
