4fd685bdb177f4c7b8e4584c905e2b3512476be2
Charles Heizer <heizer1@llnl.gov>
2017-11-15 11:12:50 -0800

    Add License banner

--- Source/MacPatch/MPFramework/MPPython.h
+++ Source/MacPatch/MPFramework/MPPython.h
@@ -1,10 +1,27 @@
 //
 //  MPPython.h
-//  PyRunner
-//
-//  Created by Charles Heizer on 11/14/17.
-//  Copyright © 2017 Charles Heizer. All rights reserved.
-//
+/*
+ Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ Produced at the Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ Written by Charles Heizer <heizer1 at llnl.gov>.
+ LLNL-CODE-636469 All rights reserved.
+ 
+ This file is part of MacPatch, a program for installing and patching
+ software.
+ 
+ MacPatch is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License (as published by the Free
+ Software Foundation) version 2, dated June 1991.
+ 
+ MacPatch is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU General Public
+ License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with MacPatch; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 #import <Foundation/Foundation.h>
 #include "Python/Python.h"
@@ -14,12 +31,10 @@
 @property (nonatomic, weak) NSString *virtualEnvPath;
 
 /**
- <#Description#>
+ Initialize Class with a python virtualenv
 
- @param runPySimpleScript <#runPySimpleScript description#>
- @param aScript <#aScript description#>
- @param error <#error description#>
- @return <#return value description#>
+ @param vEnvPath Virtualenv path
+ @return id
  */
 - (id)initWithVirtualEnv:(NSString *)vEnvPath;
 
