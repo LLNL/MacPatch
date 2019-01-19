696f8ad9b26dbcddb478e46761bb391ccc7fe88d
Charles Heizer <heizer1@llnl.gov>
2019-01-18 16:06:53 -0800

    Fix missing profile criteria

--- Source/Server/apps/mpapi/mac_profiles_2/routes.py
+++ Source/Server/apps/mpapi/mac_profiles_2/routes.py
@@ -39,13 +39,21 @@ class ProfilesForClient(MPResource):
 				q_result = MpOsConfigProfiles.query.filter(MpOsConfigProfiles.profileID.in_(_profileIDList)).all()
 				if q_result is not None:
 					for row in q_result:
-						_profile = {}
-						_profile['id'] = row.profileID
-						_profile['profileIdentifier'] = row.profileIdentifier
-						_profile['rev'] = row.profileRev
-						_profile['data'] = base64.b64encode(row.profileData)
-						_profile['remove'] = row.uninstallOnRemove
-						_profiles.append(_profile)
+						if row.enabled == 1:
+							res = self.evaluateCriteriaForProfile(cuuid,_clientGroupID,row.profileID,row.isglobal)
+							if res[0] == True:
+								_profile = {}
+								_profile['id'] = row.profileID
+								_profile['profileIdentifier'] = row.profileIdentifier
+								_profile['rev'] = row.profileRev
+								_profile['data'] = base64.b64encode(row.profileData)
+								_profile['remove'] = row.uninstallOnRemove
+								_profile['name'] = row.profileName
+								_profile['description'] = row.profileDescription
+								_profile['criteria'] = res[1]
+								_profiles.append(_profile)
+							else:
+								log_Debug('{} did not match criteria evaluation.'.format(row.profileID))
 
 			return {'errorno': '0', 'errormsg': '', 'result': {'data': _profiles, 'type': 'MacProfiles'}}, 200
 
@@ -57,6 +65,121 @@ class ProfilesForClient(MPResource):
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			log_Error('[ProfilesForClient][Get][Exception][Line: %d] CUUID: %s Message: %s' % (exc_tb.tb_lineno, cuuid, e.message))
 			return {'errorno': 500, 'errormsg': e.message, 'result': {'data':[], 'type': 'MacProfiles'}}, 500
+
+	#
+	def evaluateCriteriaForProfile(self, clientID, clientGroupID, profileID, isGlobal):
+		_groupID = None
+		if isGlobal:
+			_groupID = 0
+		else:
+			_groupID = clientGroupID
+
+		# Get the criteria for the policy id
+		q_result = MpOsProfilesGroupAssigned.query.filter(MpOsProfilesGroupAssigned.groupID == _groupID,
+														  MpOsProfilesGroupAssigned.profileID == profileID).first()
+
+		if q_result is not None:
+			# Get the criteria policy id for the profile for the client
+			res =  self.policyCriteriaTest(clientID,q_result.gPolicyID)
+			if res[0] == 1:
+				return (True, res[1])
+
+		return (False, [])
+
+	# Will check, SYSType:OSType:ModelType:OSVersion criteria
+	def policyCriteriaTest(self,clientID,policyID):
+
+		criteria = []
+		result = 0
+		q_result = MpOsProfilesCriteria.query.filter(MpOsProfilesCriteria.gPolicyID == policyID).all()
+		if q_result is not None:
+			c_result = MpClient.query.filter(MpClient.cuuid == clientID).first()
+			c_resultHW = MPISPHardwareOverview.query.filter(MPISPHardwareOverview.cuuid == clientID).first()
+
+			# Replace the comma in model name, so a list can use a comma
+			_model = c_resultHW.mpa_Model_Name.replace(",",".")
+			_type = "Desktop"
+			if "book".upper() in _model.upper():
+				_type = "Laptop"
+
+			for row in q_result:
+				if row.type == "SYSType":
+					if row.type_data == "*":
+						continue
+					else:
+						if _type.upper() in row.type_data.upper():
+							# Pass
+							continue
+						else:
+							log_Error("{} was not found in {} ".format(_type,row.type_data))
+							result = result + 1
+
+				elif row.type == "OSType":
+					if row.type_data == "*":
+						continue
+					else:
+						if not self.versionCheck(row.type_data, c_result.osver):
+							result = result + 1
+						continue
+
+				elif row.type == "ModelType":
+					if row.type_data == "*":
+						continue
+					else:
+						if not self.modelCheck(row.type_data, _model):
+							result = result + 1
+						continue
+
+				elif row.type == "OSVersion":
+					if row.type_data == "*":
+						continue
+					else:
+						if not self.versionCheck(row.type_data, c_result.osver):
+							result = result + 1
+						continue
+				else:
+					criteria.append({'type':row.type,'type_data':base64.b64encode(row.type_data)})
+
+		return (result,criteria)
+
+
+	def modelCheck(self, modelList, clientModel):
+		result = False
+		model_list = modelList.split(",")
+		for v in model_list:
+			if "*" in v:
+				_v = v.replace("*","")
+				if _v.upper() in clientModel.upper():
+					result = True
+					break
+			else:
+				if v in clientModel:
+					result = True
+					break
+
+		return result
+
+	def versionCheck(self, verStrList, clientOSVer):
+		result = False
+		version_list = verStrList.split(",")
+		for v in version_list:
+			if "*" in v:
+				_v = v.replace("*","")
+				if _v in clientOSVer:
+					result = True
+					break
+			# 10.14+
+			elif "+" in v:
+				_v = v.replace("+", "")
+				if LooseVersion(clientOSVer) >= LooseVersion(_v):
+					result = True
+					break
+			else:
+				if v == clientOSVer:
+					result = True
+					break
+
+		return result
 
 
 ''' ------------------------------- '''
