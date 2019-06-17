6f8d28d0cb087fe51e70ce3961b023e590d6116c
Charles Heizer <heizer1@llnl.gov>
2019-06-17 16:08:10 -0700

    Fix missing quote

--- Source/MacPatch/MPLoginAgent/MPLoginAgent/MinScanAndPatchVC.m
+++ Source/MacPatch/MPLoginAgent/MPLoginAgent/MinScanAndPatchVC.m
@@ -273,7 +273,7 @@ extern OSStatus MDSendAppleEventToSystemProcess(AEEventID eventToSend);
 			break;
 		case 2:
 			// Firmware updates are needed requiring a shutdown (halt)
-			[NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:@[@"reboot", @"-halt]];
+			[NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:@[@"reboot", @"-halt"]];
 			qlinfo(@"MPAuthPlugin issued a launchctl reboot and halt.");
 			[NSThread detachNewThreadSelector:@selector(countDownShowRebootButton) toTarget:self withObject:nil];
 			break;
