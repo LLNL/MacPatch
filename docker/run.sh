bb829b0f5339eeafd6d2479042b65421ce76cc50
Jorge Escobar <escobar6@llnl.gov>
2019-10-31 15:15:51 -0700

    update paths

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-  cd $MPSERVERBASE/apps
-  source env/api/bin/activate
+source $MPSERVERBASE/env/api/bin/activate
 
-  while ! (./mpapi.py db upgrade head); do
-    echo "The database doesn't appear to be up yet, giving it a few more seconds."
-    sleep 15
-  done
-  # ./mpapi.py db upgrade head
-  ./mpapi.py populate_db
+cd $MPSERVERBASE/apps
+while ! (./mpapi.py db upgrade head); do
+  echo "The database doesn't appear to be up yet, giving it a few more seconds."
+  sleep 15
+done
+./mpapi.py populate_db
 
-  deactivate
+deactivate
 
 supervisord --nodaemon -c $MPSERVERBASE/supervisord/supervisord.conf
