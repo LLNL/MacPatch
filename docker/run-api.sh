d53ffae886d133ce79c37f13a3c9cf838e85decc
Jorge Escobar <escobar6@llnl.gov>
2019-10-31 15:18:51 -0700

    Move api, console and nginx into separate containers

---
+++ docker/run-api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $MPSERVERBASE/apps
+while ! ("$MPSERVERBASE"/env/api/bin/python "$MPSERVERBASE"/apps/mpapi.py db upgrade head); do
+  echo "The database doesn't appear to be up yet, giving it a few more seconds."
+  sleep 15
+done
+"$MPSERVERBASE"/env/api/bin/python "$MPSERVERBASE"/apps/mpapi.py populate_db
+
+sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpapi.py
+"$MPSERVERBASE"/env/api/bin/python "$MPSERVERBASE"/apps/mpapi.py gunicorn
