b535a022111b3a7c8b5545b006b88e961f12ac6e
Jorge Escobar <escobar6@llnl.gov>
2019-11-07 17:19:01 -0800

    Add inventory and other misc tweaks.

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-cd "$MPSERVERBASE"/apps || exit 1
+cd "$MPSERVERBASE/apps" || exit 1
 
-while ! ("$MPSERVERBASE"/env/api/bin/python "$MPSERVERBASE"/apps/mpapi.py db upgrade head); do
+while ! ("$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" db upgrade head); do
   echo "The database doesn't appear to be up yet, giving it a few more seconds."
   sleep 15
 done
 
 
 if [ "$1" == "api" ]; then
-  sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpapi.py
-  "$MPSERVERBASE"/env/api/bin/python "$MPSERVERBASE"/apps/mpapi.py gunicorn
+  "$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" gunicorn -h "0.0.0.0"
 elif [ "$1" == "console" ]; then
-  sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpconsole.py
-  "$MPSERVERBASE"/env/console/bin/python "$MPSERVERBASE"/apps/mpconsole.py gunicorn
+  "$MPSERVERBASE/env/console/bin/python" "$MPSERVERBASE/apps/mpconsole.py" gunicorn -h "0.0.0.0"
+elif [ "$1" == "inventory" ]; then
+  "$MPSERVERBASE/conf/scripts/MPInventoryD.py" --config "$MPSERVERBASE/etc/siteconfig.json" --files "$MPSERVERBASE/InvData/files"
+elif [ "$1" == "asus" ]; then
+  "$MPSERVERBASE/conf/scripts/MPSUSPatchSync.py" --config "$MPSERVERBASE/etc/patchloader.json"
 else
   supervisord --nodaemon -c "$MPSERVERBASE"/supervisord/supervisord.conf
 fi
