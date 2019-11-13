75957ed3fd78314f222bccec116af156fa2d5275
Jorge Escobar <escobar6@llnl.gov>
2019-11-12 17:07:03 -0800

    Go back to using sed to change gunicorn host

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,9 @@
 
 cd "$MPSERVERBASE/apps" || exit 1
 
-while ! ("$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" db upgrade head); do
+sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpapi.py
+sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpconsole.py
+
   echo "The database doesn't appear to be up yet, giving it a few more seconds."
   sleep 15
 done
@@ -11,10 +13,10 @@ done
 
 if [ "$1" == "api" ]; then
   # run only the api service
-  "$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" gunicorn -h "0.0.0.0"
+  "$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" gunicorn
 elif [ "$1" == "console" ]; then
   # run only the console service
-  "$MPSERVERBASE/env/console/bin/python" "$MPSERVERBASE/apps/mpconsole.py" gunicorn -h "0.0.0.0"
+  "$MPSERVERBASE/env/console/bin/python" "$MPSERVERBASE/apps/mpconsole.py" gunicorn
 elif [ "$1" == "inventory" ]; then
   # run only the inventory service
   "$MPSERVERBASE/conf/scripts/MPInventoryD.py" --config "$MPSERVERBASE/etc/siteconfig.json" --files "$MPSERVERBASE/InvData/files"
