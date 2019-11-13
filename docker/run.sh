5e79f027a13d627f77e8f22bf77d7b9065ae8062
Jorge Escobar <escobar6@llnl.gov>
2019-11-12 17:07:31 -0800

    hide errors while waiting for db to come up

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,7 @@ cd "$MPSERVERBASE/apps" || exit 1
 sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpapi.py
 sed -i "s/host='127.0.0.1'/host='0.0.0.0'/" /opt/MacPatch/Server/apps/mpconsole.py
 
+while ! ("$MPSERVERBASE/env/api/bin/python" "$MPSERVERBASE/apps/mpapi.py" db upgrade head 2> /dev/null); do
   echo "The database doesn't appear to be up yet, giving it a few more seconds."
   sleep 15
 done
