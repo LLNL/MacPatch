c90a1a676ba85c300f0f4349a15b09bf9c37aedb
Charles Heizer <heizer1@llnl.gov>
2019-12-05 15:13:08 -0800

    Update worker class

--- Source/Server/apps/mpconsole.py
+++ Source/Server/apps/mpconsole.py
@@ -46,7 +46,7 @@ class GunicornServer(Command):
 					'bind': '{0}:{1}'.format(host, port),
 					'workers': workers,
 					'daemon': daemon,
-					'worker_class': 'gevent',
+					'worker_class': 'meinheld.gmeinheld.MeinheldWorker',
 					'preload_app': True,
 					'accesslog': '/opt/MacPatch/ServerConfig/logs/console_access.log',
 					'errorlog': '/opt/MacPatch/ServerConfig/logs/console_error.log',
