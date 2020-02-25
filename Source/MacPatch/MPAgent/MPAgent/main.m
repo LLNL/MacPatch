f2474d7008db343ee7125a80d0d7e8fbd6d26a1f
Charles Heizer <heizer1@llnl.gov>
2020-02-25 14:24:52 -0800

    Update version number

--- Source/MacPatch/MPAgent/MPAgent/main.m
+++ Source/MacPatch/MPAgent/MPAgent/main.m
@@ -37,7 +37,7 @@
 #include <getopt.h>
 #include <unistd.h>
 
-#define APPVERSION	@"3.3.4.5"
+#define APPVERSION	@"3.5.0.1"
 #define APPNAME		@"MPAgent"
 // This Define will be modified durning MPClientBuild script
 #define APPBUILD	@"[BUILD]"
