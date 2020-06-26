13d0ef50a6d6bf9c2d010dd82fc75b32d5b5cc24
Charles Heizer <heizer1@llnl.gov>
2020-06-26 11:52:49 -0700

    Update Version Number Fix Bug In agent config data API

--- Source/MacPatch/MPAgent/MPAgent/main.m
+++ Source/MacPatch/MPAgent/MPAgent/main.m
@@ -37,7 +37,7 @@
 #include <getopt.h>
 #include <unistd.h>
 
-#define APPVERSION	@"3.3.6.1"
+#define APPVERSION	@"3.3.6.2"
 #define APPNAME		@"MPAgent"
 // This Define will be modified durning MPClientBuild script
 #define APPBUILD	@"[BUILD]"
