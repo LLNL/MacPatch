3ca55341f513bb13e4301eeda6d7f86b51aa96c6
Charles Heizer <heizer1@llnl.gov>
2021-02-26 10:12:34 -0800

    Update API for Provision Task

--- Source/Server/apps/mpapi/software_4/routes.py
+++ Source/Server/apps/mpapi/software_4/routes.py
@@ -14,7 +14,7 @@ from .. model import *
 from .. mplogger import *
 from .. wsresult import *
 from .. extensions import cache
-#from .. shared.software import *
+from .. shared.software import *
 
 parser = reqparse.RequestParser()
 
@@ -75,6 +75,140 @@ class SoftwareTasksForGroup(MPResource):
 			message=str(e.args[0]).encode("utf-8")
 			log_Error('[SoftwareTasksForGroup][Get][Exception][Line: {}] CUUID: {} Message: {}'.format(exc_tb.tb_lineno, clientID, message))
 			return {'errorno': 500, 'errormsg': message, 'result': {}}, 500
+
+class SoftwareProvisionDataForTaskID(MPResource):
+
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		super(SoftwareProvisionDataForTaskID, self).__init__()
+
+	def get(self, taskID, cuuid):
+
+		wsResult = WSResult()
+		wsData = WSData()
+		wsData.data = {}
+		wsData.type = 'ProvisionTask'
+		wsResult.result = wsData
+
+		try:
+			if not isValidClientID(cuuid):
+				log_Error('[SoftwareProvisionDataForTaskID][Get]: Failed to verify ClientID (%s)' % (cuuid))
+				return wsResult.resultNoSignature(errorno=424, errormsg='Failed to verify ClientID'), 424
+
+			if not isValidSignature(self.req_signature, cuuid, self.req_uri, self.req_ts):
+				log_Error('[SoftwareProvisionDataForTaskID][Get]: Failed to verify Signature for client (%s)' % (cuuid))
+				return wsResult.resultNoSignature(errorno=424, errormsg='Failed to verify Signature'), 424
+
+			log_Debug("[SoftwareProvisionDataForTaskID][Get][%s]: Args: taskID=%s" % (cuuid, taskID))
+			q_task = MpProvisionTask.query.filter(MpProvisionTask.tuuid == taskID).first()
+
+			# Main SW Task Data
+			task = {}
+			if q_task is not None:
+				_task = SWTask()
+				_task_data = _task.struct()
+
+				for t in list(_task.keys()):
+					if t in q_task.__dict__:
+						t_Val = eval("q_task." + t)
+						if isinstance(t_Val, datetime.datetime):
+							_task_data[t] = t_Val.strftime("%Y-%m-%d %H:%M:%S")
+						elif isinstance(t_Val, datetime.date):
+							_task_data[t] = t_Val.strftime("%Y-%m-%d %H:%M:%S")
+						else:
+							_task_data[t] = t_Val
+					else:
+						# We have a few fields that dont match the model
+						# This needs to be fixed in the future
+						if t == "suuid":
+							_task_data['suuid'] = q_task.primary_suuid
+						elif t == "id":
+							_task_data['id'] = q_task.tuuid
+
+				task = _task_data
+
+			# Software Section of Task
+			q_software = MpSoftware.query.filter(MpSoftware.suuid == task['suuid']).first()
+			if q_software is not None:
+				_sw = Software()
+				_sw_data = _sw.struct()
+				for s in list(_sw.keys()):
+					if s in q_software.__dict__:
+						s_Val = eval("q_software." + s)
+						if isinstance(s_Val, datetime.datetime):
+							_sw_data[s] = s_Val.strftime("%Y-%m-%d %H:%M:%S")
+						elif isinstance(s_Val, datetime.date):
+							_sw_data[s] = s_Val.strftime("%Y-%m-%d %H:%M:%S")
+						else:
+							if s == "sw_pre_install" or s == "sw_post_install" or s == "sw_uninstall":
+								_sw_data[s] = base64.b64encode(s_Val).decode('utf-8')
+							else:
+								_sw_data[s] = s_Val
+					else:
+						if s == "vendorUrl":
+							_sw_data['vendorUrl'] = eval("q_software.sVendorURL")
+
+						elif s == "description":
+							_sw_data['description'] = eval("q_software.sDescription")
+
+						elif s == "vendor":
+							_sw_data['vendor'] = eval("q_software.sVendor")
+
+						elif s == "name":
+							_sw_data['name'] = eval("q_software.sName")
+
+						elif s == "state":
+							_sw_data['state'] = eval("q_software.sState")
+
+						elif s == "reboot":
+							_sw_data['reboot'] = eval("q_software.sReboot")
+
+						elif s == "version":
+							_sw_data['version'] = eval("q_software.sVersion")
+
+						elif s == "sid":
+							_sw_data['sid'] = eval("q_software.suuid")
+
+						elif s == "sw_post_install":
+							_sw_data['sw_post_install'] = b64EncodeAsString(rowWithDefault(q_software,"sw_post_install_script",defaultValue=''),defaultValue='')
+
+						elif s == "sw_uninstall":
+							_sw_data['sw_uninstall'] = b64EncodeAsString(rowWithDefault(q_software,"sw_uninstall_script",defaultValue=''),defaultValue='')
+
+						elif s == "sw_pre_install":
+							_sw_data['sw_pre_install'] = b64EncodeAsString(rowWithDefault(q_software,"sw_pre_install_script",defaultValue=''),defaultValue='')
+
+				task['Software'] = _sw_data
+
+			# Software Criteria Section of Task
+			q_software_cri = MpSoftwareCriteria.query.filter(MpSoftwareCriteria.suuid == task['suuid']).order_by(MpSoftwareCriteria.type_order.asc()).all()
+			if q_software_cri is not None:
+				_sw_cri = SoftwareCritera()
+				_sw_cri_data = _sw_cri.struct()
+				for row in q_software_cri:
+					if row.type == "OSType":
+						_sw_cri_data['os_type'] = row.type_data
+
+					elif row.type == "OSArch":
+						_sw_cri_data['arch_type'] = row.type_data
+
+					elif row.type == "OSVersion":
+						_sw_cri_data['os_vers'] = row.type_data
+
+				task['SoftwareCriteria'] = _sw_cri_data
+
+			# Return Result
+			wsResult.errorno = 200
+			wsResult.errormsg = ''
+			wsData.data = task
+			wsResult.data = wsData.toDict()
+			return wsResult.resultWithSignature(), 200
+
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			message=str(e.args[0]).encode("utf-8")
+			log_Error('[SoftwareProvisionDataForTaskID][Get][Exception][Line: {}] CUUID: {} Message: {}'.format(exc_tb.tb_lineno, cuuid, message))
+			return wsResult.resultNoSignature(errorno=500, errormsg=message), 500
 
 # ----------------------------------
 # Private
@@ -224,5 +358,7 @@ def swPackageCriteriaNew(swID, criteria):
 	return None
 
 # Add Routes Resources
-software_4_api.add_resource(SoftwareTasksForGroup,		'/sw/tasks/<string:clientID>/<string:groupName>')
-software_4_api.add_resource(SoftwareTasksForGroup,		'/sw/tasks/<string:clientID>/<string:groupName>/<string:osver>', endpoint='swTasksFilter')
+software_4_api.add_resource(SoftwareTasksForGroup,				'/sw/tasks/<string:clientID>/<string:groupName>')
+software_4_api.add_resource(SoftwareTasksForGroup,				'/sw/tasks/<string:clientID>/<string:groupName>/<string:osver>', endpoint='swTasksFilter')
+
+software_4_api.add_resource(SoftwareProvisionDataForTaskID,		'/sw/provision/task/<string:taskID>/<string:cuuid>')
